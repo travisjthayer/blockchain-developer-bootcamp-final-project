676ede4fd471a50ff7f5c17e16e6c39b8b1e2a89
Travis Thayer <travisthayer@traviss-mini-2.attlocal.net>
2021-11-23 10:57:23 -0500

    updates

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@
  require('dotenv').config();
  const path = require("path");
  
- const infuraKey = process.env.infuraKey;
+ const infuraUrl = 'https://ropsten.infura.io/v3/process.env.infuraKey';
  const mnemonic = process.env.mnemonic;
  
  module.exports = {
@@ -63,7 +63,11 @@
      // Useful for deploying to a public network.
      // NB: It's important to wrap the provider as a function.
      ropsten: {
-     provider: () => new HDWalletProvider(mnemonic, infuraKey),
+     provider: () => 
+          new HDWalletProvider(
+            process.env.MNEMONIC, 
+            'https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}'
+          ),
      network_id: 3,       // Ropsten's id
      gas: 5500000,        // Ropsten has a lower block limit than mainnet
      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
